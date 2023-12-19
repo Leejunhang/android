644e6cfc3dbafe1b7b0e5ecfbd671fc45210453f
Leejunhang <share97@naver.com>
2023-12-19 18:28:16 +0900

    ver14

---
+++ ex07/app/src/main/java/com/example/ex07/ChatVO.java
@@ -0,0 +1,42 @@
+package com.example.ex07;
+
+public class ChatVO {
+    private String key;
+    private String email;
+    private String date;
+    private String content;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
